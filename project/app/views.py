c036b5a885a4307772b73af0ce5b0485047d2a72
Artifice-219 <johnphilliplormalbas@gmail.com>
2025-01-03 06:14:08 -0800

    system can now create random passwords, logic courtesy of chatGpt

--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-import string
+import string, random
 
 # Create your views here.
 def index(request):
@@ -9,9 +9,11 @@ def index(request):
 # this is the part where you should generate a random password
 def get_password(request):
     uppercase = list(string.ascii_uppercase)
-    lowercase = list(string.ascii_uppercase)
+    lowercase = list(string.ascii_lowercase)
     special_chars = list(string.punctuation)
-    password = 'this youre password you piece of shit'
+
+    pool = uppercase + lowercase + special_chars
+    password = ''.join(random.choice(pool) for _ in range(14))
 
     return JsonResponse({
         'password' : password
